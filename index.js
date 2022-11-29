f628c5828d32a18d4b3c2bcb5f46e3445b02a357
Joti <tanjilajoti@gmail.com>
2022-11-29 16:31:54 +0600

    add mongodb & category

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // const jwt = require('jsonwebtoken');
-// require('dotenv').config();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -12,7 +12,36 @@ app.use(cors());
 app.use(express.json());
 
 
-// run().catch(console.log);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijmmtxg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categories = client.db('recycled_books').collection('categories');
+        // Use Aggregate to query multiple collection and then merge data
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const options = await categories.find(query).toArray();
+
+            // // get the bookings of the provided date
+            // const bookingQuery = { appointmentDate: date }
+            // const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+            // // code carefully :D
+            // options.forEach(option => {
+            //     const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+            //     const bookedSlots = optionBooked.map(book => book.slot);
+            //     const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+            //     option.slots = remainingSlots;
+            // })
+            res.send(options);
+        });
+
+    }
+    finally { }
+}
+run().catch(console.log);
 
 app.get('/', async (req, res) => {
     res.send('recycle books server is running');
